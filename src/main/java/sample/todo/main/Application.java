d1d5764e729320b6173760863d9dd427f554b210
bobsantosjr <bob.santosjr@gmail.com>
2014-12-23 04:52:41 +0800

    changed basepackages

--- src/main/java/sample/todo/main/Application.java
+++ src/main/java/sample/todo/main/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+@ComponentScan(basePackages={"sample.todo"})
 @EnableAutoConfiguration
 public class Application {
 	public static void main(String[] args) {
